4bda854deb95b9f6e02fd5623565347d4c2b14c9
Serhii Tymoshenko <me@tymoshenko.dev>
2025-11-27 12:08:14 +0100

    Configured Hilt and added properties to git ignore

--- app/src/main/java/dev/tymoshenko/mashit/MainActivity.kt
+++ app/src/main/java/dev/tymoshenko/mashit/MainActivity.kt
@@ -7,16 +7,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import dev.tymoshenko.mashit.nav.graphs.appGraph
 import dev.tymoshenko.mashit.nav.routes.AppRoutes
 import dev.tymoshenko.mashit.ui.theme.MashItTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/dev/tymoshenko/mashit/MashItApp.kt
@@ -0,0 +1,7 @@
+package dev.tymoshenko.mashit
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MashItApp : Application()
